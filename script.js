9b85c2f3b74e31e20ddc505ff9dd0db883848da2
Navdeep Singh <navvysingh07@gmail.com>
2025-02-19 21:48:46 +0530

    final formula change

--- script.js
+++ script.js
@@ -1,36 +1,56 @@
 
 function calculateAttendance(totalClasses, attendedClasses, requiredPercentage) {
-    if (totalClasses <= 0 || attendedClasses < 0 || attendedClasses > totalClasses) {
+    if (totalClasses >= 0 && attendedClasses > 0 && attendedClasses <= totalClasses) {
+        let attendancePercentage = (attendedClasses / totalClasses) * 100;
+        let requiredClasses = Math.ceil((((requiredPercentage) / 100) * totalClasses - attendedClasses) / (1 - (requiredPercentage) / 100));
+        let notrequiredClasses = Math.floor((attendedClasses - ((requiredPercentage) / 100) * totalClasses) / ((requiredPercentage) / 100));
+        
+        let classesNeededMessage = "";
+        if (attendancePercentage < requiredPercentage) {
+            classesNeededMessage = "Required classes are: " + (requiredClasses) + " to maintain " + requiredPercentage + "% attendance!";
+        }
+        else if (attendancePercentage == requiredPercentage) {
+            classesNeededMessage = "You are exactly at " + requiredPercentage + "% attendance!";
+        }
+        else {
+            classesNeededMessage = "Classes You can skip are: " + (notrequiredClasses) + " to maintain " + requiredPercentage + "% attendance!";
+        }
+    
+        return {
+            currentPercentage: attendancePercentage.toFixed(2) + "%",
+            classesNeeded: classesNeededMessage
+        };
+    }
+
+    else {
+
         return "Invalid input. Please enter valid numbers.";
     }
 
-    let attendancePercentage = (attendedClasses / totalClasses) * 100;
-    let requiredClasses = (((requiredPercentage) / 100) * totalClasses - attendedClasses) / (1 - (requiredPercentage ) / 100);
-    let notrequiredClasses = (attendedClasses - ((requiredPercentage) / 100) * totalClasses) / ((requiredPercentage) / 100);
-
-    let classesNeededMessage = "";
-    if (requiredClasses+1 > 0) {
-        classesNeededMessage = "Required classes are: " + Math.ceil(requiredClasses) + " to maintain " + requiredPercentage + "% attendance!";
-    } else if (requiredClasses+1 === 0) {
-        classesNeededMessage = "You are exactly at " + requiredPercentage + "% attendance!";
-    } else {
-        classesNeededMessage = "Classes You can skip are: " + Math.floor(notrequiredClasses) + " to maintain " + requiredPercentage + "% attendance!";
-    }
-
-    return {
-        currentPercentage: attendancePercentage.toFixed(2) + "%",
-        classesNeeded: classesNeededMessage
-    };
-
 }
+const button = document.getElementById("clacbtn"); 
+
+window.addEventListener("keydown", (event) => {
+
+  if (event.key === "Enter") {
+
+    button.click(); 
+
+  }
+
+});
 
 function calculate() {
-    let totalClasses = Number(document.getElementById("totalClasses").value);
-    let attendedClasses = Number(document.getElementById("attendedClasses").value);
     let requiredPercentage = Number(document.getElementById("PercentageReq").value);
+    let attendedClasses = Number(document.getElementById("attendedClasses").value);
+    let totalClasses = Number(document.getElementById("totalClasses").value);
 
     let result = calculateAttendance(totalClasses, attendedClasses, requiredPercentage);
-    document.getElementById("result").innerHTML = "Current Attendance: " + result.currentPercentage + "<br>" + result.classesNeeded;
+    if (typeof result === "string") {
+        document.getElementById("result").innerHTML = result;
+    } else {
+        document.getElementById("result").innerHTML ="Current percentage is: " + result.currentPercentage + "<br>" + result.classesNeeded;
+    }
 }
 
 
